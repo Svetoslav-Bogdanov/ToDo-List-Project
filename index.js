05c4fd7e37f33c23cf65acb088eb501476aaea17
slavi <shimmy@abv.bg>
2021-08-02 22:24:05 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function onButtonClick() {
+    var task = document.getElementById("newTask").value;
+    
+    if(task.length === 0){
+        alert("Полето е задължително!");
+    }else{
+        var itemsContainers = document.getElementById("listItems");
+        itemsContainers.insertAdjacentHTML("beforeend", '<li>'+ task +'</li>');
+        document.getElementById("newTask").value = "";
+    }
+}
